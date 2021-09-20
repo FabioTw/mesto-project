9dc34f240409cd74107a1f278402d683bed504a3
FabioTw <e.cherwoo@gmail.com>
2021-09-20 17:56:46 +0300

    v1.1 js fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,94 +1,98 @@
-const popup = document.querySelector('.popup_editElement');
-const popup__container = popup.querySelector('.popup__container');
-const popup__profileEdit = popup__container.querySelector('.popup__profile-edit');
-const popup__input = popup__profileEdit.querySelector('.popup__input');
-const popup__inputName = popup__input.querySelector('.popup__input-field_type_name');
-const popup__inputDescription = popup__input.querySelector('.popup__input-field_type_description');
+const popupProfile = document.querySelector('.popup_editElement');
+const popupProfileContainer = popupProfile.querySelector('.popup__container');
+const popupProfileEdit = popupProfileContainer.querySelector('.popup__profile-edit');
+const popupProfileInput = popupProfileEdit.querySelector('.popup__input');
+const popupProfileInputName = popupProfileInput.querySelector('.popup__input-field_type_name');
+const popupProfileInputDescription = popupProfileInput.querySelector('.popup__input-field_type_description');
 const popupAddElement = document.querySelector('.popup_addElement');
-const popupAddElement__container = popupAddElement.querySelector('.popup__container');
-const popupAddElement__profileEdit = popupAddElement__container.querySelector('.popup__profile-edit');
-const popupAddElement__input = popupAddElement__profileEdit.querySelector('.popup__input');
-const popupAddElement__inputName = popupAddElement__input.querySelector('.popup__input-field_type_name');
-const popupAddElement__inputDescription = popupAddElement__input.querySelector('.popup__input-field_type_description');
+const popupAddElementContainer = popupAddElement.querySelector('.popup__container');
+const popupAddElementProfileEdit = popupAddElementContainer.querySelector('.popup__profile-edit');
+const popupAddElementInput = popupAddElementProfileEdit.querySelector('.popup__input');
+const popupAddElementInputName = popupAddElementInput.querySelector('.popup__input-field_type_name');
+const popupAddElementInputDescription = popupAddElementInput.querySelector('.popup__input-field_type_description');
 const popupPhoto = document.querySelector('.popup_picture');
 const popupPhotoImg = popupPhoto.querySelector('.popup__picture-img');
 const popupPhotoText = popupPhoto.querySelector('.popup__picture-text');
 
 const profile = document.querySelector('.profile');
-const profile__author = profile.querySelector('.profile__author');
-const profile__container = profile__author.querySelector('.profile__container');
-const profile__info = profile__container.querySelector('.profile__info');
-const profile__name = profile__info.querySelector('.profile__name');
-const profile__description = profile__container.querySelector('.profile__description');
+const profileAuthor = profile.querySelector('.profile__author');
+const profileContainer = profileAuthor.querySelector('.profile__container');
+const profileInfo = profileContainer.querySelector('.profile__info');
+const profileName = profileInfo.querySelector('.profile__name');
+const profileDescription = profileContainer.querySelector('.profile__description');
 const elements = document.querySelector('.elements');
 const element = elements.querySelector('.element');
-const element__description = element.querySelector('.element__description');
+const elementDescription = element.querySelector('.element__description');
 
 const addButton = profile.querySelector('.profile__add-button');
-const editButton = profile__info.querySelector('.profile__edit-button');
-const closePopupButton = popup.querySelector('.popup__close');
+const editButton = profileInfo.querySelector('.profile__edit-button');
+const closePopupButton = popupProfile.querySelector('.popup__close');
 const closePopupAddElementButton = popupAddElement.querySelector('.popup__close');
 const closePopupPhoto = popupPhoto.querySelector('.popup__close');
-const submitButton = popup__profileEdit.querySelector('.popup__button');
-const submitAddElementButton = popupAddElement__profileEdit.querySelector('.popup__button');
+const submitButton = popupProfileEdit.querySelector('.popup__button');
+const submitAddElementButton = popupAddElementProfileEdit.querySelector('.popup__button');
 
-standartElements('Карачаевск','./images/karachevsk.png');
-standartElements('Гора Эльбрус','./images/elbrus.png');
-standartElements('Домбай','./images/dombai.png');
-standartElements('Гора Эльбрус','./images/elbrus.png');
-standartElements('Домбай','./images/dombai.png');
-standartElements('Карачаево-Ч...','./images/karachevsk.png');
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: './images/karachevsk.png'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elbrus.png'
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombai.png'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elbrus.png'
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombai.png'
+  },
+  {
+    name: 'Карачаево-Ч...',
+    link: './images/karachevsk.png'
+  }
+]; 
 
-editButton.addEventListener('click', profileEditPopup);
-submitButton.addEventListener('click', profileEditSubmitButton);
+initialCards.forEach(value => {
+  elements.append(standartElements(value.name, value.link));
+});
+
+editButton.addEventListener('click', editProfilePopup);
+submitButton.addEventListener('click', editProfileSubmitButton);
 submitAddElementButton.addEventListener('click', addElement);
-addButton.addEventListener('click', AddElementPopup);
-closePopupButton.addEventListener('click', profileEditPopup);
-closePopupAddElementButton.addEventListener('click', AddElementPopup);
+addButton.addEventListener('click', addElementPopup);
+closePopupButton.addEventListener('click', editProfilePopup);
+closePopupAddElementButton.addEventListener('click', addElementPopup);
 closePopupPhoto.addEventListener('click', popupPhotoClose);
-popup__inputName.value = profile__name.textContent;
-popup__inputDescription.value = profile__description.textContent;
+popupProfileInputName.value = profileName.textContent;
+popupProfileInputDescription.value = profileDescription.textContent;
 
-function profileEditPopup() {
-  popup.classList.toggle('popup_opened');
+function editProfilePopup() {
+  switchPopup(popupProfile);
 }
 
-function AddElementPopup() {
-  popupAddElement.classList.toggle('popup_opened');
+function addElementPopup() {
+  switchPopup(popupAddElement);
 }
 
-function profileEditSubmitButton (event) {
+function editProfileSubmitButton (event) {
   event.preventDefault(); 
-  profile__name.textContent = popup__inputName.value;
-  profile__description.textContent = popup__inputDescription.value;
-  popup.classList.toggle('popup_opened');
+  profileName.textContent = popupProfileInputName.value;
+  profileDescription.textContent = popupProfileInputDescription.value;
+  switchPopup(popupProfile);
 }
 
 function addElement (event) {
   event.preventDefault();
-  const elementTemplate = document.querySelector('#element-template').content;
-  const elementCard = elementTemplate.querySelector('.element').cloneNode(true);
-  const deleteButton = elementCard.querySelector('.element__delete');
-  
-  elementCard.querySelector('.element__text').textContent = popupAddElement__inputName.value;
-  elementCard.querySelector('.element__photo').src = popupAddElement__inputDescription.value; 
-  elementCard.querySelector('.element__photo').addEventListener('click', function(evt) {
-    popupPhoto.classList.toggle('popup_opened');
-    popupPhotoImg.src = elementCard.querySelector('.element__photo').src;
-    popupPhotoText.textContent = elementCard.querySelector('.element__text').textContent;
-  });
-  elementCard.querySelector('.element__button').addEventListener('click', function(evt) {
-    evt.target.classList.toggle('element__button_activated');
-  });
-  deleteButton.addEventListener('click', function () {
-    const listItem = deleteButton.closest('.element');
-    listItem.remove();
-  }); 
-  elements.append(elementCard); 
-  popupAddElement.classList.toggle('popup_opened');
-
-  popupAddElement__inputName.value = '';
-  popupAddElement__inputDescription.value = '';
+  elements.insertBefore(standartElements(popupAddElementInputName.value, popupAddElementInputDescription.value), element);
+  switchPopup(popupAddElement);
+  resetPopupFields (popupAddElementInputName, popupAddElementInputDescription);
 }
 
 function standartElements(name, link) {
@@ -98,9 +102,11 @@ function standartElements(name, link) {
 
   elementCard.querySelector('.element__text').textContent = name;
   elementCard.querySelector('.element__photo').src = link; 
+  elementCard.querySelector('.element__photo').alt = name + ' фото'; 
   elementCard.querySelector('.element__photo').addEventListener('click', function(evt) {
-    popupPhoto.classList.toggle('popup_opened');
+    switchPopup(popupPhoto);
     popupPhotoImg.src = link;
+    popupPhotoImg.alt = name + ' фото';
     popupPhotoText.textContent = name;
   });
   elementCard.querySelector('.element__button').addEventListener('click', function(evt) {
@@ -111,9 +117,18 @@ function standartElements(name, link) {
     const listItem = deleteButton.closest('.element');
     listItem.remove();
   }); 
-  elements.append(elementCard);
+  return elementCard;
 }
 
 function popupPhotoClose() {
-  popupPhoto.classList.toggle('popup_opened');
+  switchPopup(popupPhoto);
+}
+
+function switchPopup(popup) {
+  popup.classList.toggle('popup_opened');
+}
+
+function resetPopupFields (field1, field2) {
+  field1.value = '';
+  field2.value = '';
 }
